e45ad7502836e5142f5860bad3807d0870fbac06
aryan-prog <mandelakalam12364@gmail.com>
2025-04-19 21:11:41 +0530

    File Uploading Functionality completed

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
-    domains: ['img.freepik.com'], 
+    domains: ['img.freepik.com', 'cloud.appwrite.io'], 
   },
 };
 
